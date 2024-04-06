1be69da89867531fe989b0df8d5fdf176832e4b8
iyaTreesaBaby <diyababy1010@gmail.com>
2024-04-06 23:58:59 +0530

    Search Recipe  completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './component/AddRecipe';
+import SearchRecipe from './component/SearchRecipe';
 
 function App() {
   return (
     <div >
       <AddRecipe/>
+      <SearchRecipe/>
     </div>
   );
 }
