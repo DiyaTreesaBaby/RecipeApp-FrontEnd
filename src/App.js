492cf2b5d53cad0fe71bb4b7c14e8ecde7772542
iyaTreesaBaby <diyababy1010@gmail.com>
2024-04-08 21:20:39 +0530

    shopping card view completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './component/AddRecipe';
 import SearchRecipe from './component/SearchRecipe';
+import AllRecipe from './component/AllRecipe';
 
 function App() {
   return (
     <div >
       <AddRecipe/>
       <SearchRecipe/>
+      <AllRecipe/>
     </div>
   );
 }
