7f029752f475834ea044d93accb306f6caac7926
iyaTreesaBaby <diyababy1010@gmail.com>
2024-04-17 22:48:20 +0530

    dynamic data in AllRecipe

--- src/component/AddNavbar.jsx
+++ src/component/AddNavbar.jsx
@@ -6,9 +6,9 @@ const AddNavbar = () => {
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-success">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">RECIPIO</a>
+    <a class="navbar-brand" href="/">RECIPIO</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
